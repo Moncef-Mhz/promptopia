b898ea9489dea80567c11de3dfcdeba392791063
moncef2006 <73278962+moncef2006@users.noreply.github.com>
2023-07-03 12:18:39 +0100

    Versioning untracked files

---
+++ app/api/prompt/route.js
@@ -0,0 +1,16 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+export const GET = async (request) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate("creator");
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (err) {
+    return new Response("Faild to fetch all prompts", {
+      status: 500,
+    });
+  }
+};
